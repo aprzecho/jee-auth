3588e8b2d4181fa227c4c15d407863f9ba8a117d
aprzecho <a.przechodzki@gmail.com>
2018-08-30 10:20:59 +0200

    poprawki graficzne

--- src/main/java/com/aprzecho/jee/controller/ToolbarController.java
+++ src/main/java/com/aprzecho/jee/controller/ToolbarController.java
@@ -1,6 +1,10 @@
 package com.aprzecho.jee.controller;
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -13,15 +17,21 @@ public class ToolbarController {
 
 	@Inject
 	SessionRepository sessionRepository;
-	
+
 	@Inject
 	PageContext pageContext;
-	
+
 	public boolean getUserLogged() {
 		return pageContext.getFacesContext().getExternalContext().getUserPrincipal() != null;
 	}
-	
+
 	public String getCurrentUsername() {
 		return pageContext.getFacesContext().getExternalContext().getRemoteUser();
+	}
+
+	public void logout() throws IOException {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		ec.invalidateSession();
+		ec.redirect(ec.getRequestContextPath());
 	}
 }
